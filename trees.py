e03ef39ff280c008057639aa21027fdbe17fde8c
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-04-25 16:32:52 +0530

    done

--- trees.py
+++ trees.py
@@ -12,4 +12,14 @@ root.left.left = Node(0)
 
 print(root.left.data)
 
+
+'''
+Types of Binary Tree
+• Full Binary Tree - A full Binary tree is a special type of binary tree in which every parent node/internal node has either two or no children.
+• Perfect Binary Tree - A perfect binary tree is a type of binary tree in which every internal node has exactly two child nodes and all the leaf nodes tare at the same level.
+• Complete Binary Tree - A complete binary tree is just like a full binary tree, but with two major differences
+Every level must be completely filled, All the leaf elements must lean towards the left. The last leaf element might not have a right sibling i.e. a complete binary tree doesn't have to be a full binary tree.
+• Degenerate / Skewed Binary Tree
+• Balanced Binary Tree - It is a type of binary tree in which the difference between the height of the left and the right subtree for each node is either 0 or 1.
+'''
         
