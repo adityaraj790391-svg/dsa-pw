bb0033192c63d9a2866c9b3dc83e7a53267ad443
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-04-25 16:04:54 +0530

    done

--- trees.py
+++ trees.py
@@ -1,4 +1,15 @@
 class Node:
     def __init__(self,data):
-        
+        self.data = data
+        self.left = None
+        self.right = None
+
+root = Node(5)
+root.left =  Node(3)
+root.right = Node(9)
+root.left.right = Node(1)
+root.left.left = Node(0)
+
+print(root.left.data)
+
         
